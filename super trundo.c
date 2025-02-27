9574f1008d7a45ab8a89a219f5c4bb0395062e86
GuilhermeCampos21 <guilhermefcampos2015@gmail.com>
2025-02-26 21:01:48 -0300

    Update super trundo.c

--- super trundo.c
+++ super trundo.c
@@ -1,107 +1,113 @@
     #include <stdio.h>
 
-int main(){
-    char estado;
+    int main(){
 
-    char codigo_da_carta[100];
+    char A[50];
 
-    char nome_da_cidade[100];
+    char A01[200];
 
-    int População;
+    char nome_da_cidade[200];
+
+    int Populaçao;
 
     float Area_em_km;
 
-    float PIB;
+    int PIB;
 
     int pontos_Turisticos;
 
+    printf("Carta 1");
+
+    printf("\n"); 
 
     printf("Diga-me um estado?: \n");
-    scanf("%s", &estado);
+    scanf("%s", &A);
 
     printf("Diga-me o codigo da cidade?: \n");
-    scanf("%s", &codigo_da_carta);
+    scanf("%s", &A01);
 
     printf("Diga-me uma cidade?: \n");
     scanf("%s", &nome_da_cidade);
 
-    printf("Diga-me a população da cidade selecionada?: \n");
-    scanf("%d", &População);
+    printf("Diga-me a população da cidade selecionada: \n");
+    scanf("%d", &Populaçao);
 
     printf("Diga-me a área em km² da sua cidade?: \n");
     scanf("%f", &Area_em_km);
 
     printf("Diga-me o PIB da cidade selecionada?: \n");
-    scanf("%f", &PIB);
+    scanf("%d", &PIB);
 
     printf("Diga-me quantos pontos turisticois tem a cidade selecionada/: \n");
     scanf("%d", &pontos_Turisticos);
 
     // __________________________________________________________________________
-    printf("\n");
+    printf("\n"); //Para pular espaço
+    
+    printf("Estado: %s\n", &A);
+
+    printf("Codigo da cidade: %s\n", &A01);
+
+    printf("cidade: %s\n", nome_da_cidade);
+
+    printf("População: %d\n", Populaçao);
+
+    printf("Área: %.2f\n", Area_em_km);
+
+    printf("PIB: %d\n", PIB);
+
+    printf("Numero de Pontos Turisticos: %d\n", pontos_Turisticos);
+
+
+    //______________________________________________________
+    
+    printf("\n"); //Para pular espaço
 
     printf("Sua carta esta pronta!\n");
 
     printf("\n");
 
-    printf("Estado: %s\n", &estado);
+    printf("Carta 2");
 
-    printf("Codigo da cidade: %s\n", codigo_da_carta);
+    printf("\n"); //Para pular espaço
 
-    printf("cidade: %s\n", nome_da_cidade);
-
-    printf("População: %d\n", População);
-
-    printf("Área: %.2f\n", Area_em_km);
-
-    printf("PIB: %d\n", PIB);
-
-    printf("Numero de Pontos Turisticos: %d\n", pontos_Turisticos);
-
-    printf("\n");
-
-    printf("Proxima carta");
-    // _________________________________________________________________________
-    printf("\n");
-    
     printf("Diga-me um estado?: \n");
-    scanf("%s", &estado);
+    scanf("%s", &A);
 
-    printf("Diga-me o codigo da cidade?: \n");
-    scanf("%s", &codigo_da_carta);
+    printf("dDiga-me o codigo da cidade?: \n");
+    scanf("%s", &A01);
 
     printf("Diga-me uma cidade?: \n");
     scanf("%s", &nome_da_cidade);
 
-    printf("Diga-me a população da cidade selecionada?: \n");
-    scanf("%d", &População);
+    printf("Diga-me a população da cidade selecionada: \n");
+    scanf("%d", &Populaçao);
 
     printf("Diga-me a área em km² da sua cidade?: \n");
     scanf("%f", &Area_em_km);
 
     printf("Diga-me o PIB da cidade selecionada?: \n");
-    scanf("%f", &PIB);
+    scanf("%d", &PIB);
 
     printf("Diga-me quantos pontos turisticois tem a cidade selecionada/: \n");
     scanf("%d", &pontos_Turisticos);
 
+    printf("\n");//Para pular espaço
+
+    printf("Carta 2");
+    // __________________________________________________________________________
+
+    printf("\n"); //Para pular espaço
+
     printf("\n");
 
-    // _________________________________________________________________________
+    printf("Estado: %s\n", &A);
 
-    printf("\n");
-
-    printf("A segunda carta esta pronta!\n");
-
-    printf("\n");
-
-    printf("Estado: %s\n", &estado);
-
-    printf("Codigo da cidade: %s\n", codigo_da_carta);
+    printf("Codigo da cidade: %f\n", &A01);
 
     printf("cidade: %s\n", nome_da_cidade);
 
-    printf("População: %d\n", População);
+    printf("População: %d\n", Populaçao);
 
     printf("Área: %.2f\n", Area_em_km);
 
@@ -109,6 +115,11 @@ int main(){
 
     printf("Numero de Pontos Turisticos: %d\n", pontos_Turisticos);
 
+    printf("\n"); //Para pular espaço
+
+    printf("Sua carta esta pronta!\n");
+    
+}
 
 
 
